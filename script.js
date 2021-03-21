93f2795e00d5ec461b6784c6e664cc47241d6274
Catalin2092 <mihalcea.catalin20@gmail.com>
2021-03-21 23:03:18 +0200

    workin 90

--- script.js
+++ script.js
@@ -18,11 +18,42 @@ async function getQuote() {
         } else {
             authorText.innerText = data.quoteAuthor;
         }
+        // Reduce font size for long quotes
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }   
         quoteText.innerText = data.quoteText;
     } catch (error) {
         getQuote();
     }
 }
+
+function tweetQuote () {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+function facebookShare () {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const facebookUrl = `http://www.facebook.com/sharer.php?u=&${window.location.href}&t=${quote} - ${author}`;
+    window.open(facebookUrl, '_blank');
+}
+
+function facebookShare(descr, winWidth, winHeight) {
+    var winTop = (screen.height / 2) - (winHeight / 2);
+    var winLeft = (screen.width / 2) - (winWidth / 2);
+    window.open('http://www.facebook.com/sharer.php?s=100&p[title]=' + document.title + '&p[summary]=' + quote + '-' + author + '&p[url]=' + window.location.href, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width='+winWidth+',height='+winHeight);
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+facebookBtn.addEventListener('click', facebookShare);
 
 //On Load
 getQuote();
