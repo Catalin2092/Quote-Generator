16f391a46bf20da5a526deeab2135978938df7d0
Catalin2092 <mihalcea.catalin20@gmail.com>
2021-03-22 12:20:26 +0200

    final

--- script.js
+++ script.js
@@ -4,9 +4,25 @@ const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const facebookBtn = document.getElementById('facebook');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+//Show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden=true;
+}
+
+// Hide Loading
+function complete() {
+    if (!loading.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
 
 // Get Quote From API
 async function getQuote() {
+    loading();
     const proxyUrl = 'https://obscure-eyrie-36566.herokuapp.com/';
     const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
@@ -25,6 +41,8 @@ async function getQuote() {
             quoteText.classList.remove('long-quote');
         }   
         quoteText.innerText = data.quoteText;
+        // Stop Loader, Show Quote
+        complete();
     } catch (error) {
         getQuote();
     }
